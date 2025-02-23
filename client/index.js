75bce31eb5d01c1f585cfd4c579b6ba9b862daa7
enjesh <enjesh@gmail.com>
2025-02-24 00:01:34 +0300

    change user id and websocket name

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 const WebSocket = require('ws');
-const ws = new WebSocket('ws://mvp-control-1:8080/ws');
+const ws = new WebSocket('ws://control:8080/ws');
 
 ws.on('open', function open() {
   const payload = {
-    userID: 'user-major',
+    userID: 'user-lizzy',
     commitHash: 'ef66f332efd861a3882c42b88e55ee6c07ae9210',
     repoURL: 'http://44.243.236.139/backend.im/backendim-fastapi-app.git'
   };
